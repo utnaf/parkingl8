bad57182f96de32497296aa52fd96133bbaea705
dfantuzzi <davide.fantuzzi@trivago.com>
2018-06-20 15:22:36 +0200

    docs

--- docs/api/api_project.js
+++ docs/api/api_project.js
@@ -1,15 +1,1 @@
-define({
-  "title": "Apidoc for ParkingL8",
-  "version": "1.0.0",
-  "description": "Here the doc for the API",
-  "name": "ParkingL8",
-  "sampleUrl": false,
-  "defaultVersion": "0.0.0",
-  "apidoc": "0.3.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2018-06-19T21:35:53.583Z",
-    "url": "http://apidocjs.com",
-    "version": "0.17.6"
-  }
-});
+define({  "title": "Apidoc for ParkingL8",  "version": "1.0.0",  "description": "Here the doc for the API",  "name": "ParkingL8",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-06-20T13:22:08.531Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
